614bc59fdbdce15f896381f918324a4ff42b52e5
fusan <gogyaru@gmail.com>
2015-05-06 01:37:04 +0900

    db adress

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var url = 'mongodb://localhost/list';
+var url = 'mongodb://52.68.99.80/list';
 var db = mongoose.createConnection(url, function(err, res) {
 	if(err) {
 		console.log('error connected:' + url + '-' + err);
