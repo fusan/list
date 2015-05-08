7b02f24280b1dc65f3b3ec49c5079e096b7e264b
fusan <gogyaru@gmail.com>
2015-05-08 12:08:02 +0900

    Update model.js

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var url = 'mongodb://fusan:nobu1230@52.68.17.158/list';
+var url = 'mongodb://localhost/list';
 var db = mongoose.createConnection(url, function(err, res) {
 	if(err) {
 		console.log('error connected:' + url + '-' + err);
